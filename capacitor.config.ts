e616f00b55902e4b904019119721cc6409d55f11
SebaMax097 <SebaMax097@gmail.com>
2024-10-26 10:38:51 -0300

    Primer Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SebastianRamirez21565527K',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+  
+  variableUsuarios: any[] = []
+
+  
+  constructor( private apiService: ApiService) {}
+  ngOnInit(): void {
+    const usuarios = localStorage.getItem('usuarios')
+    if(usuarios){
+      this.variableUsuarios = JSON.parse('usuarios')
+      console.log('Informacion de la LocalStorage ',this.variableUsuarios)
+    }else{
+      this.llamarFuncionService()
+      console.log('Informacion de el servicio ',this.variableUsuarios)
+      localStorage.setItem('usuarios',JSON.stringify(this.variableUsuarios))
+    }
+  }
+
+  // Se llama la funcion de Service que se contacta con la API y asignamos la informacion a una variable local
+  llamarFuncionService(){
+    this.apiService.obtenerData().subscribe( data =>{
+      this.variableUsuarios = data
+      console.log(this.variableUsuarios)
+    })
+
+
+  }
+}
